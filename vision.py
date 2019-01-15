e58f6966f101d64be11c76a430b249ff5773e6e3
jcksnmdy <38597347+jcksnmdy@users.noreply.github.com>
2019-01-15 18:42:10 -0500

    Created

--- vision.py
+++ vision.py
@@ -7,17 +7,6 @@ import numpy as np
 def callback(value):
     pass
 
-
-def setup_trackbars(range_filter):
-    cv2.namedWindow("Trackbars", 0)
-
-    for i in ["MIN", "MAX"]:
-        v = 0 if i == "MIN" else 255
-
-        for j in range_filter:
-            cv2.createTrackbar("%s_%s" % (j, i), "Trackbars", v, 255, callback)
-
-
 def get_arguments():
     ap = argparse.ArgumentParser()
     ap.add_argument('-f', '--filter', required=True,
@@ -32,14 +21,6 @@ def get_arguments():
     return args
 
 
-def get_trackbar_values(range_filter):
-    values = []
-
-    for i in ["MIN", "MAX"]:
-        for j in range_filter:
-            v = cv2.getTrackbarPos("%s_%s" % (j, i), "Trackbars")
-            values.append(v)
-    return values
 
 
 def main():
@@ -48,8 +29,6 @@ def main():
     range_filter = args['filter'].upper()
 
     camera = cv2.VideoCapture(0)
-
-    setup_trackbars(range_filter)
 
     while True:
         if args['webcam']:
@@ -63,18 +42,26 @@ def main():
             else:
                 frame_to_thresh = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-        v1_min, v2_min, v3_min, v1_max, v2_max, v3_max = get_trackbar_values(range_filter)
+        v1_min = 31
+        v2_min = 60
+        v3_min = 100
+        v1_max = 91
+        v2_max = 254
+        v3_max = 254
 
         thresh = cv2.inRange(frame_to_thresh, (v1_min, v2_min, v3_min), (v1_max, v2_max, v3_max))
 
-        kernel = np.ones((5,5),np.uint8)
+        kernel = np.ones((1,1),np.uint8)
         mask = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
         mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+        dimensions = frame_to_thresh.shape
 
         # find contours in the mask and initialize the current
         # (x, y) center of the ball
         cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2]
         center = None
+        cv2.circle(image, (319, 239), 3, (0, 0, 255), -1)
+
  
         # only proceed if at least one contour was found
         if len(cnts) > 0:
@@ -92,7 +79,6 @@ def main():
                 # then update the list of tracked points
                 cv2.circle(image, (int(x), int(y)), int(radius),(0, 255, 255), 2)
                 cv2.circle(image, center, 3, (0, 0, 255), -1)
-                cv2.putText(image,"centroid", (center[0]+10,center[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.4,(0, 0, 255),1)
                 cv2.putText(image,"("+str(center[0])+","+str(center[1])+")", (center[0]+10,center[1]+15), cv2.FONT_HERSHEY_SIMPLEX, 0.4,(0, 0, 255),1)
             objectradius = 4.2
             distance = (14*400.45) / radius
@@ -100,7 +86,7 @@ def main():
             dfc = (300 - center[0])
             com = ((dfc*objectradius)/14000)
             comneg = abs(com)
-            if com < 0.0234 and com > -0.0234:
+            if com < 0.01 and com > -0.01:
                 print("Just right!!")
             if com < 0:
                 print("Turn left " + str(comneg) + " degrees")
